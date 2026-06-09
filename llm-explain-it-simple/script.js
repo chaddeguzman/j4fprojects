1f12b9d0bcf1f1c703cd2c7e0412b01863726a57
Chad De Guzman <67136633+chaddeguzman@users.noreply.github.com>
2026-06-09 18:26:39 +0800

    Update API URL to use gemini-2.5-flash model

--- llm-explain-it-simple/script.js
+++ llm-explain-it-simple/script.js
@@ -4,7 +4,7 @@ document.getElementById('year').textContent = new Date().getFullYear();
 // --- YOUR GOOGLE GEMINI API KEY ---
 // backup: /gemini-2.5-flash:generateContent?key=${API_KEY}
 const API_KEY = 'AQ.Ab8RN6Jw5ibPEvf8QmYjqyiR_NL9JWuD_-HTE6wh_5I2586wBQ';
-const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=${API_KEY}`;
+const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
 
 // --- Enter Key Trigger ---
 const input = document.getElementById('termInput');
